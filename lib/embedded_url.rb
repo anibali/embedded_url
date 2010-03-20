5412924c38cbaf960bb71deb00a1746a76b1ed20
Aiden Nibali <dismaldenizen@gmail.com>
2010-03-21 10:39:03 +1100

    Tidied and commented embedded_url.rb

--- lib/embedded_url.rb
+++ lib/embedded_url.rb
@@ -1,8 +1,11 @@
 require 'rubygems'
 require 'httparty'
+require 'uri'
 
+# Iterate through libraries which add a type of embedded url and require
+# each one from the "embedded_url" folder
 %w[gist slideshare youtube vimeo jamendo].each do |lib|
-  require File.join(File.dirname(__FILE__), "embedded_url/#{lib}")
+  require File.join(File.dirname(__FILE__), "embedded_url", lib)
 end
 
 module EmbeddedURL
@@ -11,26 +14,26 @@ module EmbeddedURL
   end
   
   module ClassMethods
-
     def with_embedded(field)
-      
+      # Add instance method
       define_method("embedded_#{field}") do
-
+        # Obtain link from the appropriate record field
         link = send(field)
         
-        begin
-          Gist.new(link).to_embedded || 
-            Youtube.new(link).to_embedded || 
-            Vimeo.new(link).to_embedded || 
-            SlideShare.new(link).to_embedded ||
-            Jamendo.new(link).to_embedded ||
-            (link =~ /^http/ ? link : "http://#{link}")
-        rescue
+        return_value = nil
+        
+        # Iterate through each class representing a type of embedded object and
+        # attempt to embed link
+        [Gist, Youtube, Vimeo, SlideShare, Jamendo].each do |clazz|
+          (return_value ||= clazz.new(link).to_embedded) rescue Exception
         end
         
+        # If we can't embed the link, return it as a full URL
+        uri = URI.parse link
+        return_value ||= (uri.scheme.nil? ? "http://#{link}" : link)
+        
+        return_value
       end
-      
-    end
-
-  end
-end
+    end # End with_embedded
+  end # End ClassMethods
+end # End EmbeddedURL
