7d44078aa93904ba52056664bd1536bb5efa88f8
Aiden Nibali <dismaldenizen@gmail.com>
2010-03-21 15:44:00 +1100

    Added tests for Jamendo

--- lib/embedded_url/jamendo.rb
+++ lib/embedded_url/jamendo.rb
@@ -9,9 +9,12 @@ class Jamendo
   def to_embedded
     if url.match /jamendo.com\/(.*)\/(.*)\/(\d*)/
       lang = $1 # Language (eg "en")
-      type = $2 # Type (eg "track", "album")
+      type = $2 # Type ("track" or "album")
       id = $3   # ID
-      "<object width=\"200\" height=\"300\" classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\" codebase=\"http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,0,0\" align=\"middle\"><param name=\"allowScriptAccess\" value=\"always\" /><param name=\"wmode\" value=\"transparent\" /><param name=\"movie\" value=\"http://widgets.jamendo.com/#{lang}/#{type}/?playertype=2008&#{type}_id=#{id}\" /><param name=\"quality\" value=\"high\" /><param name=\"bgcolor\" value=\"#FFFFFF\" /><embed src=\"http://widgets.jamendo.com/#{lang}/#{type}/?playertype=2008&#{type}_id=#{id}\" quality=\"high\" wmode=\"transparent\" bgcolor=\"#FFFFFF\" width=\"200\" height=\"300\" align=\"middle\" allowScriptAccess=\"always\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.macromedia.com/go/getflashplayer\">&nbsp;</embed>&nbsp;</object>"
+      
+      if %w[track album].include? type
+        "<object width=\"200\" height=\"300\" classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\" codebase=\"http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,0,0\" align=\"middle\"><param name=\"allowScriptAccess\" value=\"always\" /><param name=\"wmode\" value=\"transparent\" /><param name=\"movie\" value=\"http://widgets.jamendo.com/#{lang}/#{type}/?playertype=2008&#{type}_id=#{id}\" /><param name=\"quality\" value=\"high\" /><param name=\"bgcolor\" value=\"#FFFFFF\" /><embed src=\"http://widgets.jamendo.com/#{lang}/#{type}/?playertype=2008&#{type}_id=#{id}\" quality=\"high\" wmode=\"transparent\" bgcolor=\"#FFFFFF\" width=\"200\" height=\"300\" align=\"middle\" allowScriptAccess=\"always\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.macromedia.com/go/getflashplayer\">&nbsp;</embed>&nbsp;</object>"
+      end
     end
   end
 end
---
+++ test/jamendo_test.rb
@@ -0,0 +1,51 @@
+require 'test_helper'
+
+class JamendoTest < Test::Unit::TestCase
+  context "An EmbeddedURL::Jamendo instance" do
+    context "with an irrelevant url" do
+      setup do
+        @jamendo = EmbeddedURL::Jamendo.new "http://www.example.com/"
+      end
+      
+      should "return nil when sent #to_embedded" do
+        assert_equal nil, @jamendo.to_embedded
+      end
+    end
+    
+    context "with an irrelevant Jamendo URL" do
+      setup do
+        # Jamendo widget does not support playing all music by an artist,
+        # therefore an artist URL is not appropriate for embedding
+        @jamendo = EmbeddedURL::Jamendo.new "http://www.jamendo.com/en/artist/857"
+      end
+      
+      should "return nil when sent #to_embedded" do
+        assert_equal nil, @jamendo.to_embedded
+      end
+    end
+    
+    context "with a relevant Jamendo album URL" do
+      setup do
+        @jamendo = EmbeddedURL::Jamendo.new "http://www.jamendo.com/en/album/4240"
+      end
+      
+      should "return a string containing markup when sent #to_embedded" do
+        result = @jamendo.to_embedded
+        assert_kind_of String, result # Should be a String
+        assert_not_equal "", result   # Shouldn't be empty
+      end
+    end
+    
+    context "with a relevant Jamendo track URL" do
+      setup do
+        @jamendo = EmbeddedURL::Jamendo.new "http://www.jamendo.com/en/track/30067"
+      end
+      
+      should "return a string containing markup when sent #to_embedded" do
+        result = @jamendo.to_embedded
+        assert_kind_of String, result # Should be a String
+        assert_not_equal "", result   # Shouldn't be empty
+      end
+    end
+  end
+end
