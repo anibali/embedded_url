65f770dc743f744be9474eb1ff0fc2266352c60f
Aiden Nibali <dismaldenizen@gmail.com>
2010-03-21 11:22:18 +1100

    Added some documentation

--- lib/embedded_url.rb
+++ lib/embedded_url.rb
@@ -8,12 +8,27 @@ require 'uri'
   require File.join(File.dirname(__FILE__), "embedded_url", lib)
 end
 
+# Include this module in an ActiveRecord model to enable access to the features
+# of Embedded URL (this is done automatically when using Rails).
 module EmbeddedURL
   def self.included(base)
+    # Add methods of ClassMethods as class methods of `base`
     base.extend(ClassMethods)
   end
   
+  # Methods in this module will be added as class methods to any object which
+  # includes EmbeddedURL.
   module ClassMethods
+    # Call this method to specify an attribute to be embedded, like so:
+    #  class Post < ActiveRecord::Base
+    #    with_embedded :link
+    #  end
+    #
+    # Now the HTML for the embedded URL may be obtained as follows:
+    #  @post.embedded_link
+    #
+    # Note: if Embedded URL does not recognize the link then the original value
+    # is returned (prefixed with 'http://' if necessary).
     def with_embedded(field)
       # Add instance method
       define_method("embedded_#{field}") do
@@ -25,7 +40,7 @@ module EmbeddedURL
         # Iterate through each class representing a type of embedded object and
         # attempt to embed link
         [Gist, Youtube, Vimeo, SlideShare, Jamendo].each do |clazz|
-          (return_value ||= clazz.new(link).to_embedded) rescue Exception
+          return_value ||= clazz.new(link).to_embedded rescue nil
         end
         
         # If we can't embed the link, return it as a full URL
