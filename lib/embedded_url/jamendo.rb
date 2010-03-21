9bcccd8d3177b4ccc18d9d87c9d6beee58919c7d
Aiden Nibali <dismaldenizen@gmail.com>
2010-03-21 16:16:53 +1100

    Added brief documentation for Jamendo

--- lib/embedded_url/jamendo.rb
+++ lib/embedded_url/jamendo.rb
@@ -1,21 +1,25 @@
 module EmbeddedURL
-class Jamendo
-  attr_accessor :url
-  
-  def initialize(url)
-    self.url = url
-  end
-  
-  def to_embedded
-    if url.match /jamendo.com\/(.*)\/(.*)\/(\d*)/
-      lang = $1 # Language (eg "en")
-      type = $2 # Type ("track" or "album")
-      id = $3   # ID
-      
-      if %w[track album].include? type
-        "<object width=\"200\" height=\"300\" classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\" codebase=\"http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,0,0\" align=\"middle\"><param name=\"allowScriptAccess\" value=\"always\" /><param name=\"wmode\" value=\"transparent\" /><param name=\"movie\" value=\"http://widgets.jamendo.com/#{lang}/#{type}/?playertype=2008&#{type}_id=#{id}\" /><param name=\"quality\" value=\"high\" /><param name=\"bgcolor\" value=\"#FFFFFF\" /><embed src=\"http://widgets.jamendo.com/#{lang}/#{type}/?playertype=2008&#{type}_id=#{id}\" quality=\"high\" wmode=\"transparent\" bgcolor=\"#FFFFFF\" width=\"200\" height=\"300\" align=\"middle\" allowScriptAccess=\"always\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.macromedia.com/go/getflashplayer\">&nbsp;</embed>&nbsp;</object>"
+
+  # Enables embedding albums and tracks from Jamendo, a free and legal music
+  # site - http://www.jamendo.com/
+  class Jamendo
+    attr_accessor :url
+    
+    def initialize(url)
+      self.url = url
+    end
+    
+    def to_embedded
+      if url.match /jamendo.com\/(.*)\/(.*)\/(\d*)/
+        lang = $1 # Language (eg "en")
+        type = $2 # Type ("track" or "album")
+        id = $3   # ID
+        
+        if %w[track album].include? type
+          "<object width=\"200\" height=\"300\" classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\" codebase=\"http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,0,0\" align=\"middle\"><param name=\"allowScriptAccess\" value=\"always\" /><param name=\"wmode\" value=\"transparent\" /><param name=\"movie\" value=\"http://widgets.jamendo.com/#{lang}/#{type}/?playertype=2008&#{type}_id=#{id}\" /><param name=\"quality\" value=\"high\" /><param name=\"bgcolor\" value=\"#FFFFFF\" /><embed src=\"http://widgets.jamendo.com/#{lang}/#{type}/?playertype=2008&#{type}_id=#{id}\" quality=\"high\" wmode=\"transparent\" bgcolor=\"#FFFFFF\" width=\"200\" height=\"300\" align=\"middle\" allowScriptAccess=\"always\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.macromedia.com/go/getflashplayer\">&nbsp;</embed>&nbsp;</object>"
+        end
       end
     end
   end
-end
+
 end
