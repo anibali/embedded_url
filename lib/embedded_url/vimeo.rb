ece1e06411e031a3a38b66f429ec3aba5e45423b
Aiden Nibali <dismaldenizen@gmail.com>
2010-05-22 09:55:38 +1000

    added some basic options for video dimensions

--- lib/embedded_url/vimeo.rb
+++ lib/embedded_url/vimeo.rb
@@ -10,7 +10,12 @@ module EmbeddedURL
     def to_embedded(options={})
       if url =~ /vimeo\.com/
         video_id = url.split("/").last
-        "<object type=\"application/x-shockwave-flash\" width=\"400\" height=\"300\" data=\"http://vimeo.com/moogaloop.swf?clip_id=#{video_id}&amp;server=vimeo.com&amp;fullscreen=1&amp;show_title=1&amp;show_byline=0&amp;show_portrait=0&amp;color=00ADEF\"><param name=\"quality\" value=\"best\" /><param name=\"allowscriptaccess\" value=\"always\" /><param name=\"allowfullscreen\" value=\"true\" /><param name=\"scale\" value=\"showAll\" /><param name=\"movie\" value=\"http://vimeo.com/moogaloop.swf?clip_id=#{video_id}&amp;server=vimeo.com&amp;fullscreen=1&amp;show_title=1&amp;show_byline=0&amp;show_portrait=0&amp;color=00ADEF\" /><embed src=\"http://www.vimeo.com/moogaloop.swf?clip_id=#{video_id}&amp;server=www.vimeo.com&amp;show_title=1&amp;show_byline=0&amp;show_portrait=0&amp;color=00ADEF&amp;fullscreen=1\" type=\"application/x-shockwave-flash\" allowfullscreen=\"true\" allowscriptaccess=\"always\" width=\"400\" height=\"300\"></embed></object>"      
+        options = {
+          :width => 400,
+          :height => 300,
+          :allow_fullscreen => true
+        }.merge options
+        "<object type=\"application/x-shockwave-flash\" width=\"#{options[:width]}\" height=\"#{options[:height]}\" data=\"http://vimeo.com/moogaloop.swf?clip_id=#{video_id}&amp;server=vimeo.com&amp;fullscreen=#{options[:allow_fullscreen] ? 1 : 0}&amp;show_title=1&amp;show_byline=0&amp;show_portrait=0&amp;color=00ADEF\"><param name=\"quality\" value=\"best\" /><param name=\"allowscriptaccess\" value=\"always\" /><param name=\"allowfullscreen\" value=\"#{options[:allow_fullscreen]}\" /><param name=\"scale\" value=\"showAll\" /><param name=\"movie\" value=\"http://vimeo.com/moogaloop.swf?clip_id=#{video_id}&amp;server=vimeo.com&amp;fullscreen=#{options[:allow_fullscreen] ? 1 : 0}&amp;show_title=1&amp;show_byline=0&amp;show_portrait=0&amp;color=00ADEF\" /><embed src=\"http://www.vimeo.com/moogaloop.swf?clip_id=#{video_id}&amp;server=www.vimeo.com&amp;show_title=1&amp;show_byline=0&amp;show_portrait=0&amp;color=00ADEF&amp;fullscreen=#{options[:allow_fullscreen] ? 1 : 0}\" type=\"application/x-shockwave-flash\" allowfullscreen=\"#{options[:allow_fullscreen]}\" allowscriptaccess=\"always\" width=\"#{options[:width]}\" height=\"#{options[:height]}\"></embed></object>"      
       end
     end
   end
--- lib/embedded_url/youtube.rb
+++ lib/embedded_url/youtube.rb
@@ -10,7 +10,11 @@ module EmbeddedURL
     def to_embedded(options={})
       if url =~ /youtube\.com/
         video_id = url.split("v=").last
-        "<object width=\"400\" height=\"325\"><param name=\"movie\" value=\"http://www.youtube.com/v/#{video_id}\"></param><param name=\"allowFullScreen\" value=\"true\"></param><embed src=\"http://www.youtube.com/v/#{video_id}\" type=\"application/x-shockwave-flash\" allowfullscreen=\"true\" width=\"400\" height=\"325\"></embed></object>"
+        options = {
+          :width => 400,
+          :height => 325
+        }.merge options
+        "<object width=\"#{options[:width]}\" height=\"#{options[:height]}\"><param name=\"movie\" value=\"http://www.youtube.com/v/#{video_id}\"></param><param name=\"allowFullScreen\" value=\"true\"></param><embed src=\"http://www.youtube.com/v/#{video_id}\" type=\"application/x-shockwave-flash\" allowfullscreen=\"true\" width=\"#{options[:width]}\" height=\"#{options[:height]}\"></embed></object>"
       end
       
     end
